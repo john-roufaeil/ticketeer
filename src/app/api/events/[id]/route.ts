2f59dd1210d9088fe4ceb6229b61ec4d5e5eb174
john-roufaeil <john.flouis1@gmail.com>
2025-05-17 15:38:49 +0300

    fix: use params correctly in events api

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
   await dbConnect();
   console.log("Fetching event with ID:", id);
   try {
@@ -21,33 +21,22 @@ export async function GET(
   }
 }
 
-// export async function GET(request: NextRequest) {
-//   const { pathname } = new URL(request.url);
-//   const id = pathname.split("/").pop();
-
-//   await dbConnect();
-//   const event = await Event.findById(id);
-
-//   if (!event) {
-//     return new Response(JSON.stringify({ error: "Event not found" }), {
-//       status: 404,
-//     });
-//   }
-
-//   return Response.json(event);
-// }
-export async function PUT(request: NextRequest) {
-  const { pathname } = new URL(request.url);
-  const id = pathname.split("/").pop();
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
   await dbConnect();
-  const data = await request.json();
+  const data = await req.json();
   const event = await Event.findByIdAndUpdate(id, data, { new: true });
   return Response.json(event);
 }
 
-export async function DELETE(request: NextRequest) {
-  const { pathname } = new URL(request.url);
-  const id = pathname.split("/").pop();
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
   await dbConnect();
   await Event.findByIdAndDelete(id);
   return Response.json({ success: true });
